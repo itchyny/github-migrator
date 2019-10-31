ef1d56de15503332e69594cf9fb756e7b9e5e7ff
itchyny <itchyny@hatena.ne.jp>
2019-10-31 22:42:30 +0900

    add tests for Migrate

--- migrator/migrator_test.go
+++ migrator/migrator_test.go
@@ -3,14 +3,62 @@ package migrator
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/itchyny/github-migrator/github"
 	"github.com/itchyny/github-migrator/repo"
 )
 
-func TestMigrator(t *testing.T) {
-	sourceCli := github.NewMockClient()
-	targetCli := github.NewMockClient()
-	source := repo.New(sourceCli, "example/source")
-	target := repo.New(targetCli, "example/target")
-	var _ Migrator = New(source, target)
+func TestMigratorMigrate(t *testing.T) {
+	source := repo.New(github.NewMockClient(
+		github.MockGetRepo(func(path string) (*github.Repo, error) {
+			return &github.Repo{
+				Name:        "source",
+				Description: "Source repository.",
+				HTMLURL:     "http://localhost/example/source",
+			}, nil
+		}),
+		github.MockListIssues(func(path string, _ *github.ListIssuesParams) github.Issues {
+			return github.IssuesFromSlice([]*github.Issue{
+				{
+					Number:  1,
+					Title:   "Example title 1",
+					State:   "closed",
+					Body:    "Example body 1",
+					HTMLURL: "http://localhost/example/source/issues/1",
+				},
+				{
+					Number:  2,
+					Title:   "Example title 2",
+					State:   "open",
+					Body:    "Example body 2",
+					HTMLURL: "http://localhost/example/source/issues/2",
+				},
+			})
+		}),
+	), "example/source")
+
+	target := repo.New(github.NewMockClient(
+		github.MockGetRepo(func(path string) (*github.Repo, error) {
+			return &github.Repo{
+				Name:        "target",
+				Description: "target repository.",
+				HTMLURL:     "http://localhost/example/target",
+			}, nil
+		}),
+		github.MockListIssues(func(path string, _ *github.ListIssuesParams) github.Issues {
+			return github.IssuesFromSlice([]*github.Issue{
+				{
+					Number:  1,
+					Title:   "Example title 1",
+					State:   "closed",
+					Body:    "Example body 1",
+					HTMLURL: "http://localhost/example/target/issues/1",
+				},
+			})
+		}),
+	), "example/target")
+
+	mig := New(source, target)
+	assert.Nil(t, mig.Migrate())
 }
