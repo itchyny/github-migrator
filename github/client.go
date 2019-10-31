69743d017d9f12efc80c11523b70e73d6ee7c787
itchyny <itchyny@hatena.ne.jp>
2019-10-31 17:47:59 +0900

    create GitHub client with root endpoint

--- github/client.go
+++ github/client.go
@@ -3,10 +3,10 @@ package github
 type Client interface {
 }
 
-func New(token string) *client {
-	return &client{token: token}
+func New(token, root string) *client {
+	return &client{token: token, root: root}
 }
 
 type client struct {
-	token string
+	token, root string
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
 	"github.com/itchyny/github-migrator/github"
@@ -10,26 +11,50 @@ import (
 const name = "github-migrator"
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
-	cli, err := createGitHubClient("GITHUB_MIGRATOR_SOURCE_TOKEN")
+func run(args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("usage: %s <source> <target>", name)
+	}
+	source, err := url.Parse(args[0])
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%#v\n", cli)
+	target, err := url.Parse(args[1])
+	if err != nil {
+		return err
+	}
+	sourceCli, err := createGitHubClient("GITHUB_MIGRATOR_SOURCE_TOKEN", urlToRoot(source))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%#v\n", sourceCli)
+	targetCli, err := createGitHubClient("GITHUB_MIGRATOR_TARGET_TOKEN", urlToRoot(target))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%#v\n", targetCli)
 	return err
 }
 
-func createGitHubClient(envName string) (github.Client, error) {
+func urlToRoot(u *url.URL) string {
+	var userinfo string
+	if u.User != nil {
+		userinfo = u.User.String() + "@"
+	}
+	return fmt.Sprintf("%s://%s%s", u.Scheme, userinfo, u.Host)
+}
+
+func createGitHubClient(envName, root string) (github.Client, error) {
 	token := os.Getenv(envName)
 	if token == "" {
 		return nil, fmt.Errorf("GitHub token not found (specify %s)", envName)
 	}
-	client := github.New(token)
+	client := github.New(token, root)
 	return client, nil
 }
