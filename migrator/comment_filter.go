4b03ab19f998283dab647883122ffd53ea35cb1a
itchyny <itchyny@hatena.ne.jp>
2019-12-17 17:18:50 +0900

    rewrite image links when the host names are not equal

--- migrator/comment_filter.go
+++ migrator/comment_filter.go
@@ -1,6 +1,7 @@
 package migrator
 
 import (
+	"net/url"
 	"regexp"
 	"strings"
 
@@ -10,8 +11,22 @@ import (
 type commentFilter func(string) string
 
 func newRepoURLFilter(sourceRepo, targetRepo *github.Repo) commentFilter {
+	sourceURL, _ := url.Parse(sourceRepo.HTMLURL)
+	targetURL, _ := url.Parse(targetRepo.HTMLURL)
+	replaceImageLinks := sourceURL.Scheme != targetURL.Scheme || sourceURL.Host != targetURL.Host
+	var imageMarkdownPattern, imageHTMLPattern *regexp.Regexp
+	if replaceImageLinks {
+		urlPatten := sourceURL.Scheme + `://` + regexp.QuoteMeta(sourceURL.Host) + `[^"<>()]+`
+		imageMarkdownPattern = regexp.MustCompile(`(?i)!\[[^]]*\]\((` + urlPatten + `)\)`)
+		imageHTMLPattern = regexp.MustCompile(`(?i)<img [^<>]*\bsrc="(` + urlPatten + `)"[^<>]*>`)
+	}
 	return commentFilter(func(src string) string {
-		return strings.ReplaceAll(src, sourceRepo.HTMLURL, targetRepo.HTMLURL)
+		src = strings.ReplaceAll(src, sourceRepo.HTMLURL, targetRepo.HTMLURL)
+		if replaceImageLinks {
+			src = imageMarkdownPattern.ReplaceAllString(src, `<a href="$1">$0</a>`)
+			src = imageHTMLPattern.ReplaceAllString(src, `<a href="$1">$0</a>`)
+		}
+		return src
 	})
 }
 
